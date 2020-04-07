dee88a24a955db8ae8eb3014e52b606e40757bdb
Yuchen Liu <liu2529508462@gmail.com>
2020-04-07 17:57:36 -0400

    reform smooth as a function

---
+++ my_smooth.m
@@ -0,0 +1,22 @@
+function smooth = my_smooth(I,sigma)
+%MY_SMOOTH Summary of this function goes here
+%   Detailed explanation goes here
+%   I -- input image
+%   sigma -- parameter
+[m,n] = size(I);
+g1 = zeros(m,1);
+g2 = zeros(n,1);
+for j=1:m
+   g1(j) = 1/(2*pi*sigma^2)*exp(-j^2/(2*sigma^2));
+end
+
+for j=1:n
+   g2(j) = exp(-j^2/(2*sigma^2)); 
+end
+
+smooth = conv2(res,g1);
+smooth = conv2(smooth, g2.');
+smooth = abs(smooth);
+smooth = smooth(1:512,1:512);
+end
+
