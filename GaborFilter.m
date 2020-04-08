825255521731c85e266ca8d923a14110d1e3dc60
weixx577 <37095547+weixx577@users.noreply.github.com>
2020-04-07 23:02:10 -0400

    Update GaborFilter.m

--- GaborFilter.m
+++ GaborFilter.m
@@ -1,6 +1,6 @@
 I = imread('texture1.gif');
 I(I==1)=255;
-figure(1);
+figure();
 imshow(I);
 sigma=8;
 theta=0.75*pi; % 135 degree
@@ -8,19 +8,25 @@ F = 0.059;
 
 res = my_gabor(I, sigma, theta, F);
 
-figure(2);
+figure();
 mesh(res);
+
+figure();
+imshow(res);
 
 sigma2 = 24;
 smooth = my_smooth(I,sigma2,res);
 
-figure(3);
+figure();
 mesh(abs(smooth));
+
+figure();
+imshow(abs(smooth));
 
 %%
 I = imread('texture2.gif');
 I(I==1)=255;
-figure(1);
+figure();
 imshow(I);
 sigma=24;
 theta=0*pi; % 0 degree
@@ -28,13 +34,55 @@ F = 0.042;
 
 res = my_gabor(I, sigma, theta, F);
 
-figure(2);
+figure();
 mesh(res);
+figure();
+imshow(res);
 
 sigma2 = 24;
 smooth = my_smooth(I,sigma2,res);
 
-figure(3);
+figure();
 mesh(abs(smooth));
+figure();
+imshow(abs(smooth));
 
 %%
+I = imread('d9d77.gif');
+%I(I==1)=255;
+figure();
+imshow(I);
+sigma=36;
+theta=0.33*pi; % 60 degree
+F = 0.063;
+
+res = my_gabor(I, sigma, theta, F);
+
+figure();
+mesh(res);
+figure();
+imshow(res);
+
+%%
+I = imread('d4d29.gif');
+%I(I==1)=255;
+figure();
+imshow(I);
+sigma = 8;
+theta = -0.28*pi; % -50.5 degree
+F = 0.6038;
+
+res = my_gabor(I, sigma, theta, F);
+
+figure();
+mesh(res);
+figure();
+imshow(res);
+
+sigma2 = 40;
+smooth = my_smooth(I,sigma2,res);
+
+figure();
+mesh(abs(smooth));
+figure();
+imshow(abs(smooth));
