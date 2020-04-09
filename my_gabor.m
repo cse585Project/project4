8ecdc14526c058a6aa99c2577c9e75870fe24797
Yuchen Liu <liu2529508462@gmail.com>
2020-04-09 19:15:51 -0400

    still using conv2

--- my_gabor.m
+++ my_gabor.m
@@ -7,61 +7,42 @@ function res = my_gabor(I, sigma, theta, F)
 [m,n] = size(I);
 I = double(I);
 H = zeros(4*sigma+1,4*sigma+1);
-h1 = zeros(4*sigma+1,1);
-h2 = zeros(4*sigma+1,1);
-for j=-2*sigma:2*sigma
-   h1(j+2*sigma+1) = 1/sqrt(2*pi*sigma^2)*exp(-j^2/(2*sigma^2))*exp(1i*2*pi*F*j*cos(theta)); 
-end
-
-for j=-2*sigma:2*sigma
-   h2(j+2*sigma+1) = 1/sqrt(2*pi*sigma^2)*exp(-j^2/(2*sigma^2))*exp(1i*2*pi*F*j*sin(theta)); 
-end
-
-% for p=1:4*sigma+1
-%     for q=1:4*sigma+1
-%         H(p,q) = (1/(2*pi*sigma^2)) ...
-%             * exp((-p^2-q^2)/(2*sigma^2)) ...
-%             * exp(1i*2*pi*F*(p*cos(theta)+q*sin(theta)));
-%     end
+% h1 = zeros(4*sigma+1,1);
+% h2 = zeros(4*sigma+1,1);
+% for j=-2*sigma:2*sigma
+%    h1(j+2*sigma+1) = 1/sqrt(2*pi*sigma^2)*exp(-j^2/(2*sigma^2))*exp(1i*2*pi*F*j*cos(theta)); 
 % end
-i_1 = zeros(size(I));
-i_2 = zeros(size(I));
-for p=2*sigma+1:m-2*sigma
-    cur=0;
-    for t=1:4*sigma+1
-       cur = cur + I(t+p-2*sigma-1,:)*h1(t);
-    end
-    i_1(p,:) = cur;
-end
-for q=2*sigma+1:n-2*sigma
-    cur = 0;
-    for t=1:4*sigma+1
-      cur = cur + i_1(:,t+q-2*sigma-1)*h2(t); 
-    end
-    i_2(:,q) = cur;
-end
-
-% tmp = padarray(I,[2*sigma,2*sigma]);
-% tmp = double(tmp);
-% [m,n] = size(tmp);
-% res = complex(zeros(m,n));
+% 
+% for j=-2*sigma:2*sigma
+%    h2(j+2*sigma+1) = 1/sqrt(2*pi*sigma^2)*exp(-j^2/(2*sigma^2))*exp(1i*2*pi*F*j*sin(theta)); 
+% end
+% 
+% i_1 = zeros(size(I));
+% i_2 = zeros(size(I));
 % for p=2*sigma+1:m-2*sigma
-%     p
-%     for q=2*sigma+1:n-2*sigma
-%         xmin = p-2*sigma;
-%         xmax = p+2*sigma;
-%         ymin = q-2*sigma;
-%         ymax = q+2*sigma;
-%         for x=xmin:xmax
-%             for y=ymin:ymax
-%                 res(p,q) = res(p,q) + tmp(x,y)*H(x-p+2*sigma+1,y-q+2*sigma+1);
-%             end
-%         end
-%     end 
+%     cur=0;
+%     for t=1:4*sigma+1
+%        cur = cur + I(t+p-2*sigma-1,:)*h1(t);
+%     end
+%     i_1(p,:) = cur;
 % end
+% for q=2*sigma+1:n-2*sigma
+%     cur = 0;
+%     for t=1:4*sigma+1
+%       cur = cur + i_1(:,t+q-2*sigma-1)*h2(t); 
+%     end
+%     i_2(:,q) = cur;
+% end
+for p=-2*sigma:2*sigma
+    for q=-2*sigma:2*sigma
+        H(p+2*sigma+1,q+2*sigma+1) = (1/(2*pi*sigma^2)) ...
+            * exp((-p^2-q^2)/(2*sigma^2)) ...
+            * exp(1i*2*pi*F*(p*cos(theta)+q*sin(theta)));
+    end
+end
 
-% res = conv2(I,H,'same');
+res = conv2(I,H,'same');
 
-res = abs(i_2);
+res = abs(res);
 end
 
--- my_smooth.m
+++ my_smooth.m
@@ -13,30 +13,31 @@ end
 for j=-2*sigma:2*sigma
    g2(j+2*sigma+1) = exp(-j^2/(2*sigma^2)); 
 end
-% G=zeros(4*sigma+1);
-% for p=1:4*sigma+1
-%     for q=1:4*sigma+1
-%         G(p,q) = (1/(2*pi*sigma^2)) ...
-%             * exp((-p^2-q^2)/(2*sigma^2));
+G=zeros(4*sigma+1);
+for p=-2*sigma:2*sigma
+    for q=-2*sigma:2*sigma
+        G(p+2*sigma+1,q+2*sigma+1) = (1/(2*pi*sigma^2)) ...
+            * exp((-p^2-q^2)/(2*sigma^2));
+    end
+end
+% i_1 = zeros(size(res));
+% i_2 = zeros(size(res));
+% for p=2*sigma+1:m-2*sigma
+%     cur=0;
+%     for t=1:4*sigma+1
+%        cur = cur + res(t+p-2*sigma-1,:)*g1(t);
 %     end
+%     i_1(p,:) = cur;
 % end
-i_1 = zeros(size(res));
-i_2 = zeros(size(res));
-for p=2*sigma+1:m-2*sigma
-    cur=0;
-    for t=1:4*sigma+1
-       cur = cur + res(t+p-2*sigma-1,:)*g1(t);
-    end
-    i_1(p,:) = cur;
-end
-for q=2*sigma+1:n-2*sigma
-    cur = 0;
-    for t=1:4*sigma+1
-      cur = cur + i_1(:,t+q-2*sigma-1)*g2(t); 
-    end
-    i_2(:,q) = cur;
-end
-
-smooth = i_2;
+% for q=2*sigma+1:n-2*sigma
+%     cur = 0;
+%     for t=1:4*sigma+1
+%       cur = cur + i_1(:,t+q-2*sigma-1)*g2(t); 
+%     end
+%     i_2(:,q) = cur;
+% end
+% 
+% smooth = i_2;
+smooth = conv2(res,G,'same');
 end
 
