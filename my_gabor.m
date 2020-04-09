4bde5a07d40723f618b005beda7d9c227c9a187b
Yuchen Liu <liu2529508462@gmail.com>
2020-04-08 21:30:58 -0400

    update function

--- my_gabor.m
+++ my_gabor.m
@@ -1,4 +1,4 @@
-function res = my_gabor(I, sigma, theta, F)
+function [res] = my_gabor(I, sigma, theta, F)
 %MY_GABOR Summary of this function goes here
 %   Detailed explanation goes here
 %   I -- input image
@@ -6,20 +6,28 @@ function res = my_gabor(I, sigma, theta, F)
 %   res -- output image
 [m,n] = size(I);
 
-h1 = zeros(m,1);
-h2 = zeros(n,1);
+h1 = zeros(4*sigma+1,1);
+h2 = zeros(1,4*sigma+1);
+% H = zeros(4*sigma+1,4*sigma+1);
 
-for j=1:m
+for j=1:4*sigma+1
    h1(j) = 1/(2*pi*sigma^2)*exp(-j^2/(2*sigma^2))*exp(1i*2*pi*F*j*cos(theta)); 
 end
 
-for j=1:n
+for j=1:4*sigma+1
    h2(j) = exp(-j^2/(2*sigma^2))*exp(1i*2*pi*F*j*sin(theta)); 
 end
 
+% for p=1:4*sigma+1
+%     for q=1:4*sigma+1
+%         H(p,q) = 1/(2*pi*sigma^2)*exp((-p^2-q^2)/(2*sigma^2))*exp(1i*2*pi*F*(p*cos(theta)+q*sin(theta)));
+%     end
+% end
+
 res = conv2(I,h1);
-res = conv2(res,h2.');
-res = abs(res);% gabor
-res = res(1:m,1:n);
+res = conv2(res,h2);
+% res1 = conv2(I,H);
+res = abs(res(1:m,1:n));
+% res1 = abs(res1(1:m,1:n));
 end
 
--- my_smooth.m
+++ my_smooth.m
@@ -4,19 +4,19 @@ function smooth = my_smooth(res,sigma)
 %   res -- result of Gabor Filter
 %   sigma -- parameter
 [m,n] = size(res);
-g1 = zeros(m,1);
-g2 = zeros(n,1);
-for j=1:m
+g1 = zeros(4*sigma+1,1);
+g2 = zeros(1,4*sigma+1);
+for j=1:4*sigma+1
    g1(j) = 1/(2*pi*sigma^2)*exp(-j^2/(2*sigma^2));
 end
 
-for j=1:n
+for j=1:4*sigma+1
    g2(j) = exp(-j^2/(2*sigma^2)); 
 end
 
 smooth = conv2(res,g1);
-smooth = conv2(smooth, g2.');
-smooth = abs(smooth);
-smooth = smooth(1:512,1:512);
+smooth = conv2(smooth, g2);
+% smooth = abs(smooth);
+smooth = smooth(1:m,1:n);
 end
 
