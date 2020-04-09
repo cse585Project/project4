8eb07f28c8541993fc8281b8d12bb93c81629641
Yuchen Liu <liu2529508462@gmail.com>
2020-04-09 19:15:08 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 %% Question 2
 % load the image and convert it from binary to grayscale image
 I = imread('texture2.gif');
-
+I(I==1)=255;
 sigma1=8;
 theta=135; % 135 degree
 F = 0.059;
@@ -10,17 +10,19 @@ sigma2=24;
 % result of Gabor Filter
 m = my_gabor(I, sigma1, theta, F);
 figure(), mesh(m);
-figure(), imshow(m, []);
+figure(), imshow(mat2gray(m));
 
 % result of smooth
 smooth = my_smooth(m, sigma2);
 figure(), mesh(smooth);
-figure(), imshow(smooth, []);
+figure(), imshow(mat2gray(smooth));
+
+plot_contour(I,smooth,5);
 
 
 %% Question 2
 % load the image and convert it from binary to grayscale image
-I = imread('texture2.gif');
+I = imread('texture1.gif');
 I(I==1)=255;
 
 sigma=24;
@@ -37,10 +39,7 @@ smooth = my_smooth(res, sigma);
 figure(), mesh(smooth);
 figure(), imshow(mat2gray(smooth));
 
-% result of smooth
-smooth = my_smooth(res, sigma);
-figure(), mesh(abs(smooth));
-figure(), imshow(abs(smooth));
+plot_contour(I,smooth,10);
 
 
 %% Question 3
@@ -56,18 +55,24 @@ res = my_gabor(I, sigma, theta, F);
 figure(), mesh(res);
 figure(), imshow(mat2gray(res));
 
+plot_contour(I,res,1.3);
+
+
 %% Question 4
 I = imread('d4d29.gif');
 sigma1 = 24;
 theta = -0.28*pi; % -50.5 degree
 F = 0.6038;
+F = 0.1;
 
 res = my_gabor(I, sigma1, theta, F);
 figure(), mesh(res);
 figure(), imshow(res,[]);
 
-sigma2 = 30;
+sigma2 = 24;
 smooth = my_smooth(res, sigma2);
 
 figure(), mesh(smooth);
 figure(), imshow(mat2gray(smooth));
+
+plot_contour(I,smooth,0.4);
